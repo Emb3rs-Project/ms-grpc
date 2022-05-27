3f6d232bbbbca62d8a10eb893e76c05295e8eb9f
David Fernandes <davidsf@pantherify.com>
2022-05-27 10:01:40 +0000

    updated stuff

--- php/Manager/StartSimulationResponse.php
+++ php/Manager/StartSimulationResponse.php
@@ -13,6 +13,10 @@ use Google\Protobuf\Internal\GPBUtil;
  */
 class StartSimulationResponse extends \Google\Protobuf\Internal\Message
 {
+    /**
+     * Generated from protobuf field <code>int32 status = 1;</code>
+     */
+    private $status = 0;
 
     /**
      * Constructor.
@@ -20,11 +24,34 @@ class StartSimulationResponse extends \Google\Protobuf\Internal\Message
      * @param array $data {
      *     Optional. Data for populating the Message object.
      *
+     *     @type int $status
      * }
      */
     public function __construct($data = NULL) {
         \GPBMetadata\Manager\Manager::initOnce();
         parent::__construct($data);
+    }
+
+    /**
+     * Generated from protobuf field <code>int32 status = 1;</code>
+     * @return int
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Generated from protobuf field <code>int32 status = 1;</code>
+     * @param int $var
+     * @return $this
+     */
+    public function setStatus($var)
+    {
+        GPBUtil::checkInt32($var);
+        $this->status = $var;
+
+        return $this;
     }
 
 }
