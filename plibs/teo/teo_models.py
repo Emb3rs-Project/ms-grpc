1d3d0af8fca7f6f337c393ee41a626b56c79125c
David Fernandes <davidsf@MSI.localdomain>
2022-07-07 15:52:24 +0100

    u

--- plibs/teo/teo_models.py
+++ plibs/teo/teo_models.py
@@ -3,4 +3,18 @@ from pydantic import Field
 from base.BaseGRPC import BaseGRPC
 
 class BuildModelOutputModel(BaseGRPC):
-  pass
+  Cost: list = Field(default=[])
+  AccumulatedNewCapacity: list = Field(default=[])
+  AccumulatedNewStorageCapacity: list = Field(default=[])
+  AnnualTechnologyEmission: list = Field(default=[])
+  ProductionByTechnology: list = Field(default=[])
+  StorageLevelTimesliceStart: list = Field(default=[])
+  TotalEmissions: list = Field(default=[])
+  DiscountedCapitalInvestmentByTechnology: list = Field(default=[])
+  DiscountedCapitalInvestmentByStorage: list = Field(default=[])
+  DiscountedSalvageValueByTechnology: list = Field(default=[])
+  DiscountedSalvageValueByStorage: list = Field(default=[])
+  TotalDiscountedFixedOperatingCost: list = Field(default=[])
+  VariableOMCost: list = Field(default=[])
+  ex_capacities: list = Field(default=[])
+  report: str = Field(default="")
