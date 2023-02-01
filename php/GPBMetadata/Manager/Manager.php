d6ed15224be7c1e5fcb6236bdee40b010cd5b453
plima <pedro.lima@pdmfc.com>
2023-02-01 21:09:53 +0000

    Updating Simulation php client

--- php/GPBMetadata/Manager/Manager.php
+++ php/GPBMetadata/Manager/Manager.php
@@ -14,26 +14,33 @@ class Manager
         if (static::$is_initialized == true) {
           return;
         }
-        $pool->internalAddGeneratedFile(hex2bin(
-            "0a8f040a156d616e616765722f6d616e616765722e70726f746f12076d61" .
-            "6e6167657222640a16537461727453696d756c6174696f6e526571756573" .
-            "7412170a0f73696d756c6174696f6e5f75756964180120012809121b0a13" .
-            "73696d756c6174696f6e5f6d6574616461746118022001280912140a0c69" .
-            "6e697469616c5f6461746118032001280922290a17537461727453696d75" .
-            "6c6174696f6e526573706f6e7365120e0a06737461747573180120012805" .
-            "22460a1c5374617274436861726163746572697a6174696f6e5265717565" .
-            "737412110a09656e746974795f696418012001280912130a0b656e746974" .
-            "795f6461746118022001280922420a1d5374617274436861726163746572" .
-            "697a6174696f6e526573706f6e736512110a09656e746974795f69641801" .
-            "20012809120e0a06726573756c7418022001280932c7010a074d616e6167" .
-            "657212540a0f537461727453696d756c6174696f6e121f2e6d616e616765" .
-            "722e537461727453696d756c6174696f6e526571756573741a202e6d616e" .
-            "616765722e537461727453696d756c6174696f6e526573706f6e73651266" .
-            "0a155374617274436861726163746572697a6174696f6e12252e6d616e61" .
-            "6765722e5374617274436861726163746572697a6174696f6e5265717565" .
-            "73741a262e6d616e616765722e5374617274436861726163746572697a61" .
-            "74696f6e526573706f6e7365620670726f746f33"
-        ));
+        $pool->internalAddGeneratedFile(
+            '
+ó
+manager/manager.protomanager"d
+StartSimulationRequest
+simulation_uuid (	
+simulation_metadata (	
+initial_data (	")
+StartSimulationResponse
+status ("F
+StartCharacterizationRequest
+	entity_id (	
+entity_data (	"B
+StartCharacterizationResponse
+	entity_id (	
+result (	"]
+UpdateSimulationRequest
+simulation_uuid (	
+simulation_metadata (	
+data (	"*
+UpdateSimulationResponse
+status (2 
+ManagerT
+StartSimulation.manager.StartSimulationRequest .manager.StartSimulationResponsef
+StartCharacterization%.manager.StartCharacterizationRequest&.manager.StartCharacterizationResponseW
+UpdateSimulation .manager.UpdateSimulationRequest!.manager.UpdateSimulationResponsebproto3'
+        , true);
 
         static::$is_initialized = true;
     }
